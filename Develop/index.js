14be7e4075cf77a406d58401e3ce31da26711793
Joe King <jking0186@gmail.com>
2020-06-13 18:00:01 -0500

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,145 @@
+// Require needed for the module
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const generatedMarkdown = require("./utils/generateMarkdown");
+const api = require("./utils/api")
+
+// Choose a license for my application
+const apache = "Licensed under the [Apache License](https://spdx.org/licenses/Apache-2.0.html).";
+const gnu    = "Licensed under the [GNU GPLv3 License](https://spdx.org/licenses/GPL-3.0-or-later.html).";
+const mit    = "Licensed under the [MIT License](https://spdx.org/licenses/MIT.html).";
+const isc    = "Licensed under the [ISC License](https://spdx.org/licenses/ISC.html).";
+
+// contributors
+const yesContributors = "If you would like to contribute to this project, please follow the [Contributor Covenant Code of Conduct](https://www.contributor-covenant.org/version/2/0/code_of_conduct/) guidelines."
+const noContributors  = "This project is currently not accepting any contributions."
+
+// array of questions for user
+
+const gitHubQuestion = [
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    }
+    ];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message:  "Please provide a brief description of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message:  "What do you need to install in order for your application to work?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message:  "How do you use your application?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message:  "How would you run tests on this project?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What type of license would you like?",
+        choices: [
+            "Apache License 2.0",
+            "GNU GPLv3",
+            "MIT",
+            "ISC",
+            "None"
+        ]
+    },
+    {
+        type: "list",
+        name: "contributors",
+        message: "Would you like other developers to contribute to your project?",
+        choices: [
+            "Yes",
+            "No"
+        ]
+    }
+]
+
+// async function to combine all data from the inquirer prompts
+async function init() {
+    try {
+
+        // ask user for their github username
+        await inquirer.prompt(gitHubQuestion).then(function(response){
+            return username = response.username;
+        });
+
+        // ask user about their project
+        await inquirer.prompt(questions).then(function(response){
+            return responses = response;
+        });
+        
+        // use username from above to make an axios call to get user's image and email
+        await api.getUser(username);
+
+
+        // add username, gitHubImage, and gitHubEmail as objects into the responses object
+        responses.username = username;
+        responses.image = gitHubImage;
+        responses.email = gitHubEmail;
+
+        // check for which license the user picked
+        if(responses.license === "Apache License 2.0"){
+            responses.license = apache;
+        } else if(responses.license === "GNU GPLv3"){
+            responses.license = gnu;
+        } else if(responses.license === "MIT"){
+            responses.license = mit;
+        } else if(responses.license === "ISC"){
+            responses.license = isc;
+        } else {
+            responses.license = "This project is currently not licensed."
+        }
+
+        // check to see if user wants contributors
+        if(responses.contributors === "Yes"){
+            responses.contributors = yesContributors;
+        } else {
+            responses.contributors = noContributors;
+        }
+
+        // write data to readme using generateMarkdown
+        writeToFile(`generatedREADME.md`, generatedMarkdown(responses));
+
+    } catch (err){
+        console.log(err);
+    }
+}
+
+// function call to initialize program
+init();
+
+
+// Write To File
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        if(err){
+            console.log(err);
+        }
+            console.log("Success!");
+    });
+}
